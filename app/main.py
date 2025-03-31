bf03eda1a9c2827ebc35a24ad8a6d198db4bae02
Illya Aleksandrov <illyaalexandrov@gmail.com>
2025-03-31 11:57:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+class Animal:
+    alive = []  # Class attribute to track all alive animals
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)  # Add instance to alive list
+
+    def __repr__(self) -> None:
+        """Modify print output of Animal.alive"""
+        return f"{{Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
+
+    def die(self) -> None:
+        """Remove dead animal from alive list"""
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    @classmethod
+    def __str__(cls) -> None:
+        """String representation for Animal.alive"""
+        return str(cls.alive)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        """Toggle the hidden attribute"""
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, prey: str) -> None:
+        """Bite a herbivore and decrease its health"""
+        if isinstance(prey, Herbivore) and not prey.hidden:
+            prey.health -= 50
+            if prey.health <= 0:
+                prey.die()
